b77eadc792e1f21ac4164a7a884688a33e2f1b41
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-10-06 04:18:41 +0200

    Adjusted the details fragment to be in portrait/landscape modes updated the gradle file to implement androidx navigation

--- app/src/main/java/udacity/fwd/project2solution/ui/MainActivity.kt
+++ app/src/main/java/udacity/fwd/project2solution/ui/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(setOf())
+        appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
